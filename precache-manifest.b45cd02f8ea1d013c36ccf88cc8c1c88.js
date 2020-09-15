d47e54fb6a97812bad0d705f28af1b808ee85196
Israel <arcos@israels-mbp.lan>
2020-09-15 08:09:03 -0400

    Updates

---
+++ precache-manifest.b45cd02f8ea1d013c36ccf88cc8c1c88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c852e3f5bdb9640f899c58df3e28ac0",
+    "url": "/movies-filter/index.html"
+  },
+  {
+    "revision": "9fd3b23268268f15e5f5",
+    "url": "/movies-filter/static/css/main.23f0d482.chunk.css"
+  },
+  {
+    "revision": "79d0e6753a6c8bcee1b4",
+    "url": "/movies-filter/static/js/2.e554d416.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movies-filter/static/js/2.e554d416.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fd3b23268268f15e5f5",
+    "url": "/movies-filter/static/js/main.4ad0a37b.chunk.js"
+  },
+  {
+    "revision": "659cc16ba00da580e251",
+    "url": "/movies-filter/static/js/runtime-main.e62fa50b.js"
+  }
+]);
